97cfee0ee268d55bffd8fa381b3ef8b679aa6810
Chanira Ranagna <81904593+chanira-rangana@users.noreply.github.com>
2022-07-03 20:42:02 +0530

    create add Book event

--- script.js
+++ script.js
@@ -18,21 +18,32 @@ class UI {
 
     const books = storedBooks;
     let htmlTemplate = ``;
-    books.forEach((book) => {
-      htmlTemplate += `
-        <tr>
-            <td>${book.title}</td>
-            <td>${book.author}</td>
-            <td>${book.isbn}</td>
-            <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-        </tr>
-    `;
+    books.forEach((book) => UI.addBook(book));
+  }
 
-      const list = document.querySelector('#book-list');
-      list.innerHTML = htmlTemplate;
-    });
+  static addBook(book) {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+       <td>${book.title}</td>
+       <td>${book.author}</td>
+       <td>${book.isbn}</td>
+       <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    `;
+    const list = document.querySelector('#book-list');
+    list.appendChild(tr);
   }
 }
 
 //Event display Book
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
+//Event form Submit (Add a Book)
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  e.preventDefault;
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+
+  const newBook = new Book(title, author, isbn);
+
+  UI.addBook(newBook);
+});
